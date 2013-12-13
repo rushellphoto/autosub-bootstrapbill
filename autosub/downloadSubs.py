1e5f0807358528024039ccf36097ed79b0e7d081
Philip <philip.holmgren@gmail.com>
2013-12-13 01:15:29 +0100

    Add-on opensubtitles.org: tries to find alternate link when found link is dead

--- autosub/downloadSubs.py
+++ autosub/downloadSubs.py
@@ -28,16 +28,13 @@ log = logging.getLogger('thelogger')
 #TODO: Remove DownloadSubs threath
 
 # Think about: keep url hardcoded here or set __init__?
-# INFO: 4 current website return zip files, but I keep the unzip command
-# in the scraper for future website scrapers without zip
-# TODO: add log. to self written part
 
 # Settings
 log = logging.getLogger('thelogger')
 
-def printHTML(url):
+def getHTML(url):
     html = urllib2.urlopen(url).read()
-    print html
+    return html
     
 def getHTMLTags(url, tagSearch):
     try:
@@ -60,7 +57,7 @@ def getHTMLTagAttrib(url, tagToSearch, attrib):
     tags = getHTMLTags(url, tagToSearch)
     if len(tags) != 1:
         log.error("downloadSubs.getHTMLTagAttrib: More than one HTML tag %s was found for %s" % (tagToSearch, url))
-        return False
+        return None
     for tag in tags:
         url = tag[attrib].strip('/')
         return url
@@ -72,12 +69,20 @@ def unzip(url):
     try:
         tmpfile = StringIO(urllib2.urlopen(req).read())
     except:
-        log.error("downloadSubs.unzip: Zip file at %s couldn't be retrieve" % url)    
-    zipfile = ZipFile(tmpfile)
+        log.error("downloadSubs.unzip: Zip file at %s couldn't be retrieved" % url)
+        return None     
+    try: 
+        zipfile = ZipFile(tmpfile)
+    except:
+        log.error("unzip: Expected a zip file but got error for link %s" % url)
+        log.debug("unzip: %s is likely a dead link, this is known for opensubtitles.org" % url)
+        return None
+
     nameList = zipfile.namelist()
     for name in nameList:
         # sometimes .nfo files are in the zip container
-        if name.endswith('.srt'):
+        tmpname = name.lower()
+        if tmpname.endswith('srt'):
             file = StringIO(zipfile.open(name).read())
             log.debug("downloadSubs.unzip: Retrieving zip file for %s was succesful" % url )
             return file
@@ -92,28 +97,49 @@ def openSubtitles(subSeekerLink):
     openSubLink = 'http://www.opensubtitles.org/subtitleserve/sub/' 
     # link is http://www.opensubtitles.org/subtitles/5318376/revolution-everyone-says-i-love-you-en
     link = getHTMLTagAttrib(subSeekerLink, 'iframe', 'src')            
-    zipUrl = openSubLink + link.split('/')[4].encode('utf8')
-    subtitleFile = unzip(zipUrl)
-    return subtitleFile
+    if link:
+        html = getHTML(link)
+        id = None
+        # capture dead links
+        if html.find("msg error"):
+            # get alternate link       
+            r = re.search('http://www.opensubtitles.org/en/subtitles/(\d*)/', html).group(1)
+            if r:
+                id = r
+            else: return None
+        else:
+            id = link.split('/')[4].encode('utf8')
+        zipUrl = openSubLink + id
+        subtitleFile = unzip(zipUrl)
+        return subtitleFile
+    else:
+        return None
     
 def undertexter(subSeekerLink):
     # http://www.engsub.net/197187
     engSub = 'http://www.engsub.net/getsub.php?id='
     # link is http://www.engsub.net/197187
     link = getHTMLTagAttrib(subSeekerLink, 'iframe', 'src')        
-    zipUrl = engSub + link.split('/')[3].encode('utf8')
-    subtitleFile = unzip(zipUrl)
-    return subtitleFile
+    if link:    
+        zipUrl = engSub + link.split('/')[3].encode('utf8')
+        subtitleFile = unzip(zipUrl)
+        return subtitleFile
+    else:
+        return None
     
 def podnapisi(subSeekerLink):
     baseLink = 'http://www.podnapisi.net/'
     tags_first = getHTMLTags(subSeekerLink, 'a')
+    if len(tags_first) == 0:
+        return None
     for tag in tags_first:
         url = tag['href'].strip('/')
         # first link: to the episode subtitle page
         if re.match(urljoin(baseLink, 'ppodnapisi/podnapis/i'), url):
             linkToPodnapisi = url
             tags_second = getHTMLTags(linkToPodnapisi, 'a')
+            if len(tags_second) == 0:
+                return None
             for tag in tags_second:
                 if tag.has_key('href'):
                     url = tag['href'].strip('/')
@@ -121,17 +147,33 @@ def podnapisi(subSeekerLink):
                     if re.search('ppodnapisi/download', url):
                         zipUrl = urljoin(baseLink,url)
                         subtitleFile = unzip(zipUrl)
-                        return subtitleFile 
+                        return subtitleFile
+                    else:
+                        log.error("Podnapisi: Something went wrong while retrieving download link")
+                        log.debug("Podnapisi: Couldnt find the download link on the Subscene page for %s" % subSeekerLink)
+                        return None
+                else:
+                    log.error("Podnapisi: Something went wrong while retrieving download link")
+                    log.debug("Podnapisi: No hrefs were found in the Subscene HTML page for %s" % subSeekerLink)
+                    return None
+        else:
+            log.error("Podnapisi: Something went wrong while retrieving download link")
+            log.debug("Podnapisi: Couldnt find the Subseeker link to the Subscene page for %s" % subSeekerLink) 
+            return None
     
 def subscene(subSeekerLink):
     baseLink = 'http://subscene.com/'
     tags_first = getHTMLTags(subSeekerLink, 'a')
+    if len(tags_first) == 0:
+        return None
     for tag in tags_first:
         url = tag['href'].strip('/')
         # first link: to the episode subtitle page
         if re.match(urljoin(baseLink, 'subtitles'), url):
             linkToSubscene = url
             tags_second = getHTMLTags(linkToSubscene, 'a')
+            if len(tags_second) == 0:
+                return None
             for tag in tags_second:
                 if tag.has_key('href'):
                     url = tag['href'].strip('/')
@@ -140,17 +182,29 @@ def subscene(subSeekerLink):
                         zipUrl = urljoin(baseLink, url)
                         subtitleFile = unzip(zipUrl)
                         return subtitleFile
+                    else:
+                        log.error("Subscene: Something went wrong while retrieving download link")
+                        log.debug("Subscene: Couldnt find the download link on the Subscene page for %s" % subSeekerLink)
+                        return None
+                else:
+                    log.error("Subscene: Something went wrong while retrieving download link")
+                    log.debug("Subscene: No hrefs were found in the Subscene HTML page for %s" % subSeekerLink)
+                    return None
+        else:
+            log.error("Subscene: Something went wrong while retrieving download link")
+            log.debug("Subscene: Couldnt find the Subseeker link to the Subscene page for %s" % subSeekerLink) 
+            return None
+        
+def DownloadSub(downloadDict):    
 
-def DownloadSub(downloadDict):
-    
-    log.debug("downloadSubs: Starting DownloadSub function")
-    
+    log.debug("downloadSubs: Starting DownloadSub function")    
+
     if 'destinationFileLocationOnDisk' in downloadDict.keys() and 'downloadLink' in downloadDict.keys():
         log.debug("downloadSubs: Download dict seems ook. Dumping it for debug: %r" % downloadDict) 
         destsrt = downloadDict['destinationFileLocationOnDisk']
         website = downloadDict['website']
-        subSeekerLink = downloadDict['downloadLink']        
-        
+        subSeekerLink = downloadDict['downloadLink']
+
         log.debug("downloadSubs: Trying to download the following subtitle %s" % subSeekerLink)
         
         destdir = os.path.split(destsrt)[0] #make sure the download dest is there
@@ -161,7 +215,7 @@ def DownloadSub(downloadDict):
             log.debug("checkSubs: no destination directory %s" %destdir)
             return False        
         
-        #Decide which scraper to use
+        # Decide which scraper to use
         # Return a fileStringIO object 
         log.info('website is %s' % website) 
         if website == 'opensubtitles.org':
@@ -185,13 +239,15 @@ def DownloadSub(downloadDict):
         #Lets first download the subtitle to a tempfile and then write it to the destination
         tmpfile = tempfile.TemporaryFile('w+b')
         
-        try:
-            tmpfile.write(fileStringIO.getvalue())
-            tmpfile.write('\n') #If subtitle has some footer which doesn't have a line feed >.>
-        except:
-            log.error("downloadSubs: Error while downloading subtitle %s. Subtitle might be corrupt %s." % (destsrt, website))
+        if fileStringIO:
+            try:
+                tmpfile.write(fileStringIO.getvalue())
+                tmpfile.write('\n') #If subtitle has some footer which doesn't have a line feed >.>
+            except:
+                log.error("downloadSubs: Error while downloading subtitle %s. Subtitle might be corrupt %s." % (destsrt, website))
+                return False
+        else:
             return False
-        
        
         tmpfile.seek(0) #Return to the start of the file
         
