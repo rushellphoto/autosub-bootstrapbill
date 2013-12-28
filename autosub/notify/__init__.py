59c231577fc3a560ac9c0db1ede2d08ea9be476a
Philip <philip.holmgren@gmail.com>
2013-12-28 19:32:11 +0100

    Adding a7 VIP Account option

---
+++ autosub/notify/__init__.py
@@ -0,0 +1,65 @@
+# Autosub notify library - http://code.google.com/p/auto-sub/
+# Every function should have 2 calls, send_notify and test_notify
+# Send notify should get 3 argument: videofile, subtitlefile (both without path) and lang (which should be the language)
+# test_notify doesn't require any argument
+# every module should return True if success, and False when failed
+
+import logging
+import os
+
+import autosub
+
+from autosub.notify import twitter
+from autosub.notify import mail
+from autosub.notify import nma
+from autosub.notify import growl
+from autosub.notify import prowl
+from autosub.notify import pushalot
+from autosub.notify import pushover
+from autosub.notify import boxcar
+
+log = logging.getLogger('thelogger')  
+
+def notify(lang, subtitlefile, videofile):
+    log.debug("Notification: Trying to send notification. Language: %s Srt: %s Video: %s" %(lang, subtitlefile, videofile))
+    #Lets strip video file and subtitle file of its path!
+    subtitlefile = os.path.basename(subtitlefile)
+    videofile = os.path.basename(videofile)
+    
+    if lang == 'English' and autosub.NOTIFYEN:
+        notifySend(lang, subtitlefile, videofile)
+    if lang == 'Dutch' and autosub.NOTIFYNL:
+        notifySend(lang, subtitlefile, videofile)
+
+def notifySend(lang, subtitlefile, videofile):
+    if autosub.NOTIFYTWITTER:
+        log.debug("Notification: Twitter is enabled")
+        twitter.send_notify(lang, subtitlefile, videofile)
+    
+    if autosub.NOTIFYMAIL:
+        log.debug("Notification: Mail is enabled")
+        mail.send_notify(lang, subtitlefile, videofile)
+    
+    if autosub.NOTIFYNMA:
+        log.debug("Notification: Notify My Android is enabled")
+        nma.send_notify(lang, subtitlefile, videofile)
+    
+    if autosub.NOTIFYGROWL:
+        log.debug("Notification: Growl is enabled")
+        growl.send_notify(lang, subtitlefile, videofile)
+
+    if autosub.NOTIFYPROWL:
+        log.debug("Notification: Prowl is enabled")
+        prowl.send_notify(lang, subtitlefile, videofile)
+    
+    if autosub.NOTIFYPUSHALOT:
+        log.debug("Notification: Pushalot is enabled")
+        pushalot.send_notify(lang, subtitlefile, videofile)
+    
+    if autosub.NOTIFYPUSHOVER:
+        log.debug("Notification: Pushover is enabled")
+        pushover.send_notify(lang, subtitlefile, videofile)
+    
+    if autosub.NOTIFYBOXCAR:
+        log.debug("Notification: Boxcar is enabled")
+        boxcar.send_notify(lang, subtitlefile, videofile)
---
+++ autosub/scanDisk.py
@@ -0,0 +1,120 @@
+# Autosub Db.py - http://code.google.com/p/auto-sub/
+#
+# The Autosub scanDisk module
+#
+
+import logging
+import os
+import platform
+import re
+import time
+
+# Autosub specific modules
+import autosub
+import autosub.Helpers
+from autosub.ProcessFilename import ProcessFilename
+# Settings
+log = logging.getLogger('thelogger')
+
+def walkDir(path):
+    for dirname, dirnames, filenames in os.walk(os.path.join(path)):
+            log.debug("scanDisk: directory name: %s" %dirname)
+            if re.search('_unpack_', dirname, re.IGNORECASE): 
+                log.debug("scanDisk: found a unpack directory, skipping")
+                continue
+            
+            if autosub.SKIPHIDDENDIRS and os.path.split(dirname)[1].startswith(u'.'):
+                continue
+            
+            if re.search('_failed_', dirname, re.IGNORECASE): 
+                log.debug("scanDisk: found a failed directory, skipping")
+                continue
+             
+
+            for filename in filenames:
+                if not platform.system() == 'Windows':
+                    tempFilename = autosub.Helpers.removeIllegalChars(filename)
+                    if not tempFilename is filename:
+                        os.rename(os.path.join(dirname, filename), os.path.join(dirname, tempFilename))
+                        log.info("scanDir: Renamed file %s" % tempFilename)
+                    filename = tempFilename
+                splitname = filename.split(".")
+                ext = splitname[len(splitname) - 1]
+
+                if ext in ('avi', 'mkv', 'wmv', 'ts', 'mp4'):
+                    if re.search('sample', filename): continue
+
+                    # What subtitle files should we expect?
+            
+                    if autosub.SUBNL != "":
+                        srtfile = os.path.splitext(filename)[0] + u"." + autosub.SUBNL + u".srt"
+                    else:
+                        srtfile = os.path.splitext(filename)[0] + u".srt"
+        
+                    srtfileeng = os.path.splitext(filename)[0] + u"." + autosub.SUBENG + u".srt"
+
+                    if not os.path.exists(os.path.join(dirname, srtfile)) or (not os.path.exists(os.path.join(dirname, srtfileeng)) and autosub.DOWNLOADENG):
+                        log.debug("scanDir: File %s is missing a subtitle" % filename)
+                        lang = []
+                        filenameResults = ProcessFilename(os.path.splitext(filename)[0], os.path.splitext(filename)[1])
+                        if 'title' in filenameResults.keys():
+                            if 'season' in filenameResults.keys():
+                                if 'episode' in filenameResults.keys():
+                                    title = filenameResults['title']
+                                    season = filenameResults['season']
+                                    episode = filenameResults['episode']
+
+                                    if autosub.Helpers.SkipShow(title, season, episode) == True:
+                                        log.debug("scanDir: SkipShow returned True")
+                                        log.info("scanDir: Skipping %s - Season %s Episode %s" % (title, season, episode))
+                                        continue
+                                    log.info("scanDir: Dutch subtitle wanted for %s and added to wantedQueue" % filename)
+                                    filenameResults['originalFileLocationOnDisk'] = os.path.join(dirname, filename)
+                                    filenameResults['timestamp'] = unicode(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(os.path.getctime(filenameResults['originalFileLocationOnDisk']))))
+                                    if not os.path.exists(os.path.join(dirname, srtfile)):
+                                        lang.append(autosub.DUTCH)
+                                    if not os.path.exists(os.path.join(dirname, srtfileeng)) and (autosub.FALLBACKTOENG or autosub.DOWNLOADENG):
+                                        lang.append(autosub.ENGLISH)
+                                    
+                                    filenameResults['lang'] = lang
+                                    autosub.WANTEDQUEUE.append(filenameResults)
+                                    
+                                else:
+                                    log.error("scanDir: Could not process the filename properly filename: %s" % filename)
+                                    continue
+                            else:
+                                log.error("scanDir: Could not process the filename properly filename: %s" % filename)
+                                continue
+                        else:
+                            log.error("scanDir: Could not process the filename properly filename: %s" % filename)
+                            continue
+
+class scanDisk():
+    """
+    Scan the specified path for episodes without Dutch or (if wanted) English subtitles.
+    If found add these Dutch or English subtitles to the WANTEDQUEUE.
+    """
+    def run(self):
+        log.debug("scanDir: Starting round of local disk checking at %s" % autosub.ROOTPATH)
+        if autosub.WANTEDQUEUELOCK == True:
+            log.debug("scanDir: Exiting, another threat is using the queues")
+            return False
+        else:
+            autosub.WANTEDQUEUELOCK = True
+        
+        autosub.WANTEDQUEUE = []
+
+        seriespaths = [x.strip() for x in autosub.ROOTPATH.split(',')]
+        for seriespath in seriespaths:
+            if not os.path.exists(seriespath):
+                log.error("scanDir: Root path %s does not exist, aborting..." % seriespath)
+                continue
+            
+            try:
+                walkDir(seriespath)
+            except:
+                walkDir(str(seriespath))
+                                    
+        log.debug("scanDir: Finished round of local disk checking")
+        autosub.WANTEDQUEUELOCK = False
+        return True
