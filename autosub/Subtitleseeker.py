5570181fd48e07fd0f3569a933b53386ac842dff
Philip <philip.holmgren@gmail.com>
2013-12-29 02:34:40 +0100

    Bug Fix: Returning subtitles with None; Added some debug prints for error hunting

---
+++ autosub/Subtitleseeker.py
@@ -0,0 +1,140 @@
+#
+# Autosub Subtitleseeker.py - http://code.google.com/p/auto-sub/
+#
+# The Subtitleseeker API module
+#
+
+import logging
+
+from xml.dom import minidom
+from operator import itemgetter
+
+import autosub.Helpers
+from autosub.ProcessFilename import ProcessFilename
+# Settings
+log = logging.getLogger('thelogger')
+
+
+def getSubLinks(showid, lang, releaseDetails):
+    """
+    Return all the hits that reach minmatchscore, sorted with the best at the top of the list
+    Each element had the downloadlink, score, releasename, and source website)
+    Matching is based on the provided release details.
+
+    Keyword arguments:
+    showid -- The IMDB id of the show
+    lang -- Language of the wanted subtitle, Dutch or English
+    releaseDetails -- Dict containing the quality, releasegrp, source season and episode.
+    """
+
+    SOURCEWEBSITES = []
+    
+    # Addic7ed release are found and scored in downloadSubs itself
+    # To get maximal sensitivity for this site        
+    if autosub.PODNAPISILANG == lang or autosub.PODNAPISILANG == 'Both':
+        SOURCEWEBSITES.append('podnapisi.net')
+        
+    if autosub.SUBSCENELANG == lang or autosub.SUBSCENELANG == 'Both':
+        SOURCEWEBSITES.append('subscene.com')
+        
+    if autosub.BIERDOPJEMIRRORLANG == lang or autosub.BIERDOPJEMIRRORLANG == 'Both':
+        SOURCEWEBSITES.append('bierdopje.eu')
+        
+    if autosub.UNDERTEXTERLANG == lang or autosub.UNDERTEXTERLANG == 'Both':
+        SOURCEWEBSITES.append('undertexter.se')
+    
+    if autosub.OPENSUBTITLESLANG == lang or autosub.OPENSUBTITLESLANG == 'Both':
+        SOURCEWEBSITES.append('opensubtitles.org')
+
+    if len(SOURCEWEBSITES) == 0:
+        return None
+    
+    api = autosub.API
+    
+    if showid == -1:
+        return None    
+    quality = None
+    releasegrp = None
+    source = None
+    season = releaseDetails['season']
+    episode = releaseDetails['episode']
+    
+    
+    
+    # this is the API search 
+    getSubLinkUrl = "%s&imdb=%s&season=%s&episode=%s&language=%s" % (api, showid, season, episode, lang)
+    log.info('Subtitleseeker: This is the subseeker API request %s' % getSubLinkUrl)
+    if autosub.Helpers.checkAPICallsSubSeeker(use=True):
+        try:
+            subseekerapi = autosub.Helpers.API(getSubLinkUrl)
+            dom = minidom.parse(subseekerapi.resp)
+            subseekerapi.resp.close()
+        except:
+            log.error("getSubLink: The server returned an error for request %s" % getSubLinkUrl)
+            return None
+    else:
+        log.error("API: out of api calls for SubtitleSeeker.com")
+    
+    if 'quality' in releaseDetails.keys(): quality = releaseDetails['quality']
+    if 'releasegrp' in releaseDetails.keys(): releasegrp = releaseDetails['releasegrp']
+    if 'source' in releaseDetails.keys(): source = releaseDetails['source']
+    if 'codec' in releaseDetails.keys(): codec = releaseDetails['codec']
+    
+    if not dom or len(dom.getElementsByTagName('item')) == 0:
+        return None
+
+    scoredict = {}
+    releasedict = {}
+    websitedict = {}
+    
+    for sub in dom.getElementsByTagName('item'):
+        release = sub.getElementsByTagName('release')[0].firstChild.data
+        release = release.lower()
+        # Remove the .srt extension some of the uploaders leave on the file
+        if release.endswith(".srt"):
+            release = release[:-4]
+        website = sub.getElementsByTagName('site')[0].firstChild.data  
+        website = website.lower() 
+        
+        if not website in SOURCEWEBSITES:
+            continue       
+
+        tmpDict = ProcessFilename(release, '')
+        if not tmpDict:
+            continue
+        
+        # Scoredict is a dictionary with a download link and its match score. This will be used to determine the best match (the highest matchscore)
+        scoredict[sub.getElementsByTagName('url')[0].firstChild.data] = autosub.Helpers.scoreMatch(tmpDict, release, quality, releasegrp, source, codec)
+    
+        # Releasedict is a dictionary with the release name, used for the lastdownload database
+        releasedict[sub.getElementsByTagName('url')[0].firstChild.data] = release
+        
+        # Websitedict is a dictionary with the download link coupled with the source website        
+        websitedict[sub.getElementsByTagName('url')[0].firstChild.data] = website
+        
+    # Done comparing all the results, lets sort them and return the highest result
+    # If there are results with the same score, the download links which comes first (alphabetically) will be returned
+    # Also check if the result match the minimal score
+    sortedscoredict = sorted(scoredict.items(), key=itemgetter(1), reverse=True)
+    toDelete = []
+    for index, item in enumerate(sortedscoredict):
+        log.debug('getSubLink: checking minimal match score for %s. Minimal match score is: %s' %(item,autosub.MINMATCHSCORE))
+        score = item[1]        
+        link = item[0]
+        if not score >= autosub.MINMATCHSCORE:
+            log.debug('getSubLink: %s does not match the minimal match score' % link)
+            toDelete.append(index)
+    i = len(toDelete) - 1
+    while i >= 0:
+        log.debug("getSubLink: Removed item from the ScoreDict at index %s" % toDelete[i])
+        sortedscoredict.pop(toDelete[i])
+        i = i - 1
+    if len(sortedscoredict) > 0:
+        allResults = []
+        j=0        
+        while j < len(sortedscoredict):
+            allResults.append(sortedscoredict[j] + (releasedict[sortedscoredict[j][0]], websitedict[sortedscoredict[j][0]]))
+            j += 1
+        return allResults
+    
+    return None
---
+++ autosub/checkSub.py
@@ -0,0 +1,104 @@
+# Autosub Db.py - http://code.google.com/p/auto-sub/
+#
+# The Autosub checkSub module
+#
+
+import logging
+import os
+
+# Autosub specific modules
+import autosub.Subtitleseeker
+import autosub.Helpers as Helpers
+from autosub.downloadSubs import DownloadSub
+
+# Settings
+log = logging.getLogger('thelogger')
+
+
+class checkSub():
+    """
+    Check the SubtitleSeeker API for subtitles of episodes that are in the WANTEDQUEUE.
+    If the subtitles are found, call DownloadSub
+    """
+    def run(self):
+        log.debug("checkSub: Starting round of checkSub")
+        toDelete_wantedQueue = []
+        if not Helpers.checkAPICallsTvdb() or not Helpers.checkAPICallsSubSeeker():            
+            log.warning("checkSub: out of api calls")
+            return True
+        
+        if autosub.WANTEDQUEUELOCK:
+            log.debug("checkSub: Exiting, another threat is using the queues")
+            return False
+        else:
+            autosub.WANTEDQUEUELOCK = True
+        
+        for index, wantedItem in enumerate(autosub.WANTEDQUEUE):
+            title = wantedItem['title']
+            season = wantedItem['season']
+            episode = wantedItem['episode']
+            originalfile = wantedItem['originalFileLocationOnDisk']
+            languages = wantedItem['lang']
+            
+            if not Helpers.checkAPICallsTvdb() or not Helpers.checkAPICallsSubSeeker():
+                #Make sure that we are allow to connect to SubtitleSeeker and MyMovie
+                log.warning("checkSub: out of api calls")
+                break
+            
+            if autosub.SUBNL != "":
+                srtfile = os.path.splitext(originalfile)[0] + u"." + autosub.SUBNL + u".srt"
+            else:
+                srtfile = os.path.splitext(originalfile)[0] + u".srt"
+
+            engsrtfile = os.path.splitext(originalfile)[0] + u"." + autosub.SUBENG + u".srt"
+            
+            #lets try to find a showid
+            showid = Helpers.getShowid(title)
+            #no showid? skip this item
+            if not showid:
+                continue
+            
+            langtmp = languages[:]
+            for lang in langtmp:
+                log.debug("checkSub: trying to get a downloadlink for %s, language is %s" % (originalfile, lang))
+                # get all links higher than the minmatch as input for downloadSub
+                
+                # Returns a list of 4-element tuples
+                allResults = autosub.Subtitleseeker.getSubLinks(showid, lang, wantedItem)                
+
+                if lang == autosub.DUTCH:
+                    wantedItem['destinationFileLocationOnDisk'] = srtfile
+                elif lang == autosub.ENGLISH:
+                    wantedItem['destinationFileLocationOnDisk'] = engsrtfile
+                    
+                if allResults:                   
+                    log.info("checkSub: The episode %s - Season %s Episode %s has 1 or more matching subtitles on SubtitleSeeker, downloading it!" % (title, season, episode))
+                    log.debug("checkSub: destination filename %s" % wantedItem['destinationFileLocationOnDisk'])
+                
+                downloadItem = wantedItem.copy()
+                downloadItem['downlang'] = lang
+                    
+                if not DownloadSub(downloadItem, allResults):
+                    continue
+                    
+                if lang == autosub.DUTCH and (autosub.FALLBACKTOENG and not autosub.DOWNLOADENG) and autosub.ENGLISH in languages:
+                    log.debug('checkSub: We found a dutch subtitle and fallback is true. Removing the english subtitle from the wantedlist.')
+                    languages.remove(autosub.ENGLISH)
+                    languages.remove(lang)
+                    if len(languages) == 0:
+                        toDelete_wantedQueue.append(index)
+                    break
+                    
+                languages.remove(lang)
+                if len(languages) == 0:
+                    toDelete_wantedQueue.append(index)
+                                        
+        i = len(toDelete_wantedQueue) - 1
+        while i >= 0:
+            log.debug("checkSub: Removed item from the wantedQueue at index %s" % toDelete_wantedQueue[i])
+            autosub.WANTEDQUEUE.pop(toDelete_wantedQueue[i])
+            i = i - 1
+
+        log.debug("checkSub: Finished round of checkSub")
+        autosub.WANTEDQUEUELOCK = False
+        return True
